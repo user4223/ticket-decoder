ef29cf2cf79339b6e1493816db72c2039dd76880
sascha <s.lumma@gmail.com>
2022-04-03 16:34:43 +0200

    Fix conan folders

--- setup.Release.sh
+++ setup.Release.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 DIR="$( cd "$(dirname "$0")" ; pwd -P )"
 BUILD_DIR=$DIR/build/Release
-CONAN_BUILD_DIR=$DIR/build/conan
 
 mkdir -p $BUILD_DIR
 pushd $BUILD_DIR
-   conan install $DIR --install-folder $CONAN_BUILD_DIR --build missing
+   conan install $DIR --build missing
    cmake $DIR -DBIN_PATH_POSTFIX=Release -DCMAKE_BUILD_TYPE=Release
    cmake --build . --config Release -- -j
 popd
