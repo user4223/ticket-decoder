da0ffcfe32e6bbb2c2dd7410ad378ecbcbdf35b1
sascha <s.lumma@gmail.com>
2022-09-06 18:06:19 +0200

    Simplify zxing library usage

--- source/lib/barcode/detail/source/AztecDecoder.cpp
+++ source/lib/barcode/detail/source/AztecDecoder.cpp
@@ -1,14 +1,6 @@
 #include "../include/AztecDecoder.h"
 
 #include "ZXing/ReadBarcode.h"
-#include "ZXing/HybridBinarizer.h"
-#include "ZXing/GlobalHistogramBinarizer.h"
-#include "ZXing/GenericLuminanceSource.h"
-#include "ZXing/DecoderResult.h"
-#include "ZXing/aztec/AZReader.h"
-#include "ZXing/aztec/AZDetector.h"
-#include "ZXing/aztec/AZDecoder.h"
-#include "ZXing/aztec/AZDetectorResult.h"
 
 #include "lib/dip/filtering/include/Transform.h"
 
@@ -21,23 +13,31 @@ namespace barcode::detail
   {
     api::Result result;
     const cv::Mat image;
-    const bool pure;
-    const bool mirrored = false;
     bool detectionFinished = false;
-    std::shared_ptr<ZXing::LuminanceSource const> source;
-    std::shared_ptr<ZXing::BitMatrix const> matrix;
-    ZXing::Aztec::DetectorResult detectorResult;
-    ZXing::DecoderResult decoderResult;
+    ZXing::DecodeHints hints;
+    ZXing::Result zresult;
 
-    Internal(api::Result &&result, cv::Mat const &i, bool p) : result(std::move(result)), image(i), pure(p)
+    Internal(api::Result &&result, cv::Mat const &i, bool p)
+        : result(std::move(result)),
+          image(dip::filtering::upscale(dip::filtering::flipX(i), 2.)),
+          detectionFinished(false),
+          hints(),
+          zresult(ZXing::DecodeStatus::NotFound)
     {
-      if (!image.empty())
+      if (image.empty())
       {
-        source = std::make_shared<ZXing::GenericLuminanceSource>(image.cols, image.rows, image.data, image.step);
-        // Depending whether our image is already binarized or not, we could use a dummy binarizer here
-        // internal->matrix = ZXing::GlobalHistogramBinarizer{internal->source}.getBlackMatrix();
-        matrix = ZXing::HybridBinarizer{source}.getBlackMatrix();
+        return;
       }
+
+      hints.setFormats(ZXing::BarcodeFormat::Aztec);
+      hints.setBinarizer(ZXing::Binarizer::FixedThreshold); // maybe on camera images we prefer LocalAverage
+      hints.setCharacterSet("ISO-8859-1");
+      hints.setIsPure(p);
+      hints.setTryRotate(true);
+      hints.setTryHarder(true);
+
+      auto const view = ZXing::ImageView{image.data, image.cols, image.rows, ZXing::ImageFormat::Lum, (int)image.step, 1};
+      zresult = ZXing::ReadBarcode(view, hints);
     }
   };
 
@@ -46,21 +46,11 @@ namespace barcode::detail
 
   api::Level AztecDecoder::detect()
   {
-    if (internal->matrix == nullptr)
-    {
-      return api::Level::Unknown;
-    }
-
-    internal->detectorResult = ZXing::Aztec::Detector::Detect(
-        *(internal->matrix),
-        internal->mirrored,
-        internal->pure);
-
     internal->detectionFinished = true;
-    internal->result.level = internal->detectorResult.isValid()
-                                 ? api::Level::Detected
-                                 : api::Level::Unknown;
-
+    if (!(internal->zresult.position().bottomRight() == ZXing::PointI{}))
+    {
+      internal->result.level = api::Level::Detected;
+    }
     return internal->result.level;
   }
 
@@ -71,38 +61,16 @@ namespace barcode::detail
       detect();
     }
 
-    if (!internal->detectorResult.isValid())
+    if (!internal->zresult.isValid())
     {
       return std::move(internal->result);
     }
 
-    internal->decoderResult = ZXing::Aztec::Decoder::Decode(internal->detectorResult, "ISO-8859-1"); // Actually it should be UTF8
-    if (!internal->decoderResult.isValid())
-    {
-      // Give it a 2nd chance with 15 degree rotated image, for whatever reason
-      auto rotated = dip::filtering::rotate(internal->image, 15.f);
-      internal = std::make_shared<Internal>(
-          api::Result(internal->result.id, internal->result.box),
-          internal->image,
-          internal->pure);
-
-      detect();
-      if (!internal->detectorResult.isValid())
-      {
-        return std::move(internal->result);
-      }
-
-      internal->decoderResult = ZXing::Aztec::Decoder::Decode(internal->detectorResult, "ISO-8859-1"); // Actually it should be UTF8
-      if (!internal->decoderResult.isValid())
-      {
-        return std::move(internal->result);
-      }
-    }
     internal->result.level = api::Level::Decoded;
 
-    internal->result.payload = std::vector<std::uint8_t>(internal->decoderResult.text().size());
-    std::transform(internal->decoderResult.text().begin(),
-                   internal->decoderResult.text().end(),
+    internal->result.payload = std::vector<std::uint8_t>(internal->zresult.text().size());
+    std::transform(internal->zresult.text().begin(),
+                   internal->zresult.text().end(),
                    internal->result.payload.begin(),
                    [](std::wstring::value_type const &v)
                    { return (uint8_t)v; });
