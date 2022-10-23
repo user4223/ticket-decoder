706371ddeafe70d415cbc74ee70e45cecc3721d6
sascha <s.lumma@gmail.com>
2022-10-23 13:47:07 +0200

    Fix return code handling

--- setup.Debug.sh
+++ setup.Debug.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 DIR="$( cd "$(dirname "$0")" ; pwd -P )"
 BUILD_DIR=$DIR/build/Debug
+RETURN_CODE=0
 
 mkdir -p $BUILD_DIR
 pushd $BUILD_DIR
    conan install $DIR -s build_type=Debug --build missing -pr=debug
    cmake -S $DIR -DCMAKE_BUILD_TYPE=Debug
    cmake --build . --config Debug -- -j
+   RETURN_CODE=$?
 popd
 
+exit $RETURN_CODE
--- setup.Release.sh
+++ setup.Release.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 DIR="$( cd "$(dirname "$0")" ; pwd -P )"
 BUILD_DIR=$DIR/build/Release
+RETURN_CODE=0
 
 mkdir -p $BUILD_DIR
 pushd $BUILD_DIR
    conan install $DIR -s build_type=Release --build missing
    cmake -S $DIR -DCMAKE_BUILD_TYPE=Release
    cmake --build . --config Release -- -j
+   RETURN_CODE=$?
 popd
 
+exit $RETURN_CODE
