1b9a28c8220a8adbd74888086e521602ab958a0b
sascha <s.lumma@gmail.com>
2022-10-23 15:05:54 +0200

    Try 2 make clang default conan compiler

--- etc/setup.Dependencies.sh
+++ etc/setup.Dependencies.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-git clone -b v1.2.0 https://github.com/nu-book/zxing-cpp
+git clone -b v1.3.0 https://github.com/nu-book/zxing-cpp
 cp conanfile.py zxing-cpp/
 cd zxing-cpp
 conan create -s build_type=Release .
