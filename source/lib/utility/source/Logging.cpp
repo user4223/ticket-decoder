17a74b8f857d0cd97eb2f222ca99b90109b558ef
sascha <s.lumma@gmail.com>
2022-10-31 19:31:55 +0100

    Set default log level 2 info

--- source/lib/utility/source/Logging.cpp
+++ source/lib/utility/source/Logging.cpp
@@ -20,6 +20,8 @@ namespace utility
     config.setToDefault();
     config.setGlobally(el::ConfigurationType::Format, "[%levshort %datetime{%Y-%M-%dT%H:%m:%s} %logger] %msg");
     el::Loggers::setDefaultConfigurations(config, true);
+    el::Loggers::setLoggingLevel(el::Level::Info);
+    el::Loggers::addFlag(el::LoggingFlag::HierarchicalLogging);
     return LoggerFactory{};
   }
 }
