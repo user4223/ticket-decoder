20e99dd211632aa7f5aef2c231ad1ac3d99a0139
sascha <s.lumma@gmail.com>
2022-11-02 22:11:12 +0100

    Try to create proper logger configuration

--- source/lib/utility/source/Logging.cpp
+++ source/lib/utility/source/Logging.cpp
@@ -16,12 +16,26 @@ namespace utility
 
   LoggerFactory LoggerFactory::create()
   {
-    el::Configurations config;
-    config.setToDefault();
-    config.setGlobally(el::ConfigurationType::Format, "[%levshort %datetime{%Y-%M-%dT%H:%m:%s} %logger] %msg");
-    el::Loggers::setDefaultConfigurations(config, true);
-    el::Loggers::setLoggingLevel(el::Level::Info);
     el::Loggers::addFlag(el::LoggingFlag::HierarchicalLogging);
+
+    auto config = el::Configurations{};
+    auto const disabled = std::string("false");
+    auto const enabled = std::string("true");
+    config.setGlobally(el::ConfigurationType::ToFile, disabled);
+    config.setGlobally(el::ConfigurationType::MaxLogFileSize, std::string("0"));
+    config.setGlobally(el::ConfigurationType::ToStandardOutput, enabled);
+    config.setGlobally(el::ConfigurationType::SubsecondPrecision, std::string("0"));
+    config.setGlobally(el::ConfigurationType::PerformanceTracking, disabled);
+    config.setGlobally(el::ConfigurationType::LogFlushThreshold, std::string("0"));
+    config.setGlobally(el::ConfigurationType::Format, "[%level %datetime{%Y-%M-%dT%H:%m:%s} %logger] %msg");
+    config.set(el::Level::Trace, el::ConfigurationType::Enabled, disabled);
+    config.set(el::Level::Debug, el::ConfigurationType::Enabled, disabled);
+    config.set(el::Level::Info, el::ConfigurationType::Enabled, enabled);
+    config.set(el::Level::Warning, el::ConfigurationType::Enabled, enabled);
+    config.set(el::Level::Error, el::ConfigurationType::Enabled, enabled);
+    config.set(el::Level::Fatal, el::ConfigurationType::Enabled, enabled);
+    el::Loggers::setDefaultConfigurations(config, true);
+
     return LoggerFactory{};
   }
 }
