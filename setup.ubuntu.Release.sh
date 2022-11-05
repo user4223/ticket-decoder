91a077c83de79c2ffa2752b8e035c281a6c9dcb9
sascha <s.lumma@gmail.com>
2022-11-05 08:12:46 +0100

    Fix ubuntu docker image build stuff

--- setup.ubuntu.Release.sh
+++ setup.ubuntu.Release.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
 docker build . -t builder -f etc/Dockerfile
-docker run -it builder
+docker run -it \
+  --mount type=bind,source="$(pwd)"/source,target=/aztec-detector/source,readonly \
+  --mount type=bind,source="$(pwd)"/CMakeLists.txt,target=/aztec-detector/CMakeLists.txt,readonly \
+  --mount type=bind,source="$(pwd)"/setup.Release.sh,target=/aztec-detector/setup.Release.sh,readonly \
+  builder
