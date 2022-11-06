de281acb2f611704b0f9bb5e5db4ab99bfb9f4e7
sascha <s.lumma@gmail.com>
2022-11-06 18:07:26 +0100

    Mount output folder to make build result usable on host system

--- setup.ubuntu.clang12.Release.sh
+++ setup.ubuntu.clang12.Release.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+mkdir -p "$(pwd)"/build/ubuntu.clang12.Release
 docker build . -t ticket-decoder-builder -f etc/Dockerfile
 docker run -it \
   --mount type=bind,source="$(pwd)"/source,target=/ticket-decoder/source,readonly \
   --mount type=bind,source="$(pwd)"/CMakeLists.txt,target=/ticket-decoder/CMakeLists.txt,readonly \
   --mount type=bind,source="$(pwd)"/setup.Release.sh,target=/ticket-decoder/setup.Release.sh,readonly \
+  --mount type=bind,source="$(pwd)"/build/ubuntu.clang12.Release,target=/ticket-decoder/build/Release \
   ticket-decoder-builder
