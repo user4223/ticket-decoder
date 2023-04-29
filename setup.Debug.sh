8397723d3af511802a8a0b8a7643fe37810356dc
sascha <s.lumma@gmail.com>
2023-04-29 21:26:02 +0200

    Use preset 4 build

--- setup.Debug.sh
+++ setup.Debug.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 conan install . -of build/Debug -pr:h ticket-decoder -pr:b ticket-decoder -s build_type=Debug --build missing
-cmake -S . -B build/Debug/ -DCMAKE_BUILD_TYPE=Debug
-cmake --build build/Debug/ --config Debug -- $@
+cmake -S . -B build/Debug/ --preset conan-debug
+cmake --build build/Debug/ --preset conan-debug -- $@
