0b5c0e8c9283ef5740d26cacdf7ce93206feaf12
sascha <s.lumma@gmail.com>
2023-12-16 18:25:08 +0100

    Fix pipe

--- setup.Debug.sh
+++ setup.Debug.sh
@@ -2,7 +2,7 @@
 
 # create a matching conan profile if it is not existing
 # conan profile new ticket-decoder --force --detect
-conan install . -if build/Debug -pr ticket-decoder -pr:b ticket-decoder -s build_type=Debug --build missing
+conan install . -if build/Debug -pr ticket-decoder -pr:b ticket-decoder -s build_type=Debug -c tools.system.package_manager:mode=install --build missing
 # when presets are working for sure due to cmake >= 3.23, use: --preset debug
 cmake -S . -B build/Debug/ -DCMAKE_TOOLCHAIN_FILE=build/Debug/generators/conan_toolchain.cmake -DCMAKE_BUILD_TYPE=Debug
 cmake --build build/Debug/ --config Debug -- $@
