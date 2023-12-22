72c94edcccf9a29663c37a60982802846e26202d
sascha <s.lumma@gmail.com>
2023-12-22 16:56:37 +0100

    Throw explaining exception in case of missing or invalid input 4 binary data

--- source/lib/utility/source/FileSystem.cpp
+++ source/lib/utility/source/FileSystem.cpp
@@ -42,6 +42,11 @@ namespace utility
 
   std::vector<std::uint8_t> readBinary(std::filesystem::path filePath)
   {
+    if (!std::filesystem::is_regular_file(filePath))
+    {
+      throw std::runtime_error(std::string("Path to load raw data from does not exist or does not point to a file: ") + filePath.string());
+    }
+
     auto ifs = std::ifstream(filePath, std::ios::binary | std::ios::ate);
     auto const size = ifs.tellg();
     ifs.seekg(0, std::ios::beg);
