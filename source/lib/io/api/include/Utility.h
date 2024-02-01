e838b395ace8e0af5f911187fa60048da5bf95e1
sascha <s.lumma@gmail.com>
2024-02-01 13:28:30 +0100

    Add minimal documentation

--- source/lib/io/api/include/Utility.h
+++ source/lib/io/api/include/Utility.h
@@ -7,8 +7,12 @@
 namespace io::api::utility
 {
 
+    /* Returns true when all given paths are directories
+     */
     bool areDirectories(std::vector<std::filesystem::path> const &paths);
 
+    /* Returns true when all given paths are regular files
+     */
     bool areFiles(std::vector<std::filesystem::path> const &paths);
 
     void checkAndEnsureInputOutputPaths(std::filesystem::path const inputPath, std::optional<std::filesystem::path> optionalOutputPath);
