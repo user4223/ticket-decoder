ed83477cdb5712b52b081d921395910a82083461
sascha <s.lumma@gmail.com>
2024-04-10 21:14:24 +0200

    Separate U_FLEX versions in different impl files

---
+++ source/lib/uic918/detail/include/RecordU_FLEX_30.h
@@ -0,0 +1,13 @@
+
+#pragma once
+
+#include "lib/utility/include/JsonBuilder.h"
+#include "lib/utility/include/LoggingFwd.h"
+
+#include <optional>
+#include <vector>
+
+namespace uic918::detail
+{
+    std::optional<::utility::JsonBuilder> convertV30(::utility::Logger &logger, std::vector<std::uint8_t> const &bytes);
+}
