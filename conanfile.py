365816a8a1ddf6d4b69b12e6fee176b47715fd2a
sascha <s.lumma@gmail.com>
2024-04-27 16:12:38 +0200

    Activate boost:ptyhon dependency

--- conanfile.py
+++ conanfile.py
@@ -20,7 +20,7 @@ class TicketDecoderConan(ConanFile):
                # https://conan.io/center/recipes/tclap
                ("tclap/1.2.5"),
                # https://conan.io/center/recipes/boost
-               ("boost/1.83.0"),              # poppler requires boost but the recipe fails when boost has to be built as dependency for poppler (probably the recipe has a bug, since even when splash is disabled, the boost requirement remains unfortunately)
+               ("boost/1.84.0"),              # poppler requires boost but the recipe fails when boost has to be built as dependency for poppler (probably the recipe has a bug, since even when splash is disabled, the boost requirement remains unfortunately)
                # Override some requirements for poppler, since it uses older dependencies than opencv for same libs
                # ("zlib/1.3", "override"),      # Remove this direct override when poppler gets updated
                # ("libpng/1.6.42", "override"),   # Remove this direct override when freetype/2.12.1 vs. opencv/4.8.1 gets updated
@@ -60,7 +60,41 @@ class TicketDecoderConan(ConanFile):
                 # "botan:with_bzip2": True,
                 # "botan:with_zlib": True,
                 # boost
-                "boost:header_only": True, # When we have this dependency due to poppler, then as small as possible, so header only libs
+                "boost:pch": False,
+                "boost:header_only": False, # When we have this dependency due to poppler, then as small as possible, so header only libs
+                "boost:without_atomic": True,
+                "boost:without_chrono": True,
+                # "boost:without_cobalt": True,
+                "boost:without_container": True,
+                "boost:without_context": True,
+                "boost:without_contract": True,
+                "boost:without_coroutine": True,
+                "boost:without_date_time": True,
+                "boost:without_exception": True,
+                "boost:without_fiber": True,
+                "boost:without_filesystem": True,
+                "boost:without_graph": True,
+                "boost:without_graph_parallel": True,
+                "boost:without_iostreams": True,
+                "boost:without_json": True,
+                "boost:without_locale": True,
+                "boost:without_log": True,
+                "boost:without_math": True,
+                "boost:without_mpi": True,
+                "boost:without_nowide": True,
+                "boost:without_program_options": True,
+                "boost:without_python": False,
+                "boost:without_random": True,
+                "boost:without_regex": True,
+                "boost:without_serialization": True,
+                "boost:without_stacktrace": True,
+                "boost:without_system": True,
+                "boost:without_test": True,
+                "boost:without_thread": True,
+                "boost:without_timer": True,
+                "boost:without_type_erasure": True,
+                "boost:without_url": True,
+                "boost:without_wave": True,
                 # poppler
                 "poppler:fontconfiguration": "fontconfig",
                 "poppler:with_lcms": False,
@@ -73,7 +107,7 @@ class TicketDecoderConan(ConanFile):
                 "poppler:with_tiff": False,
                 "poppler:with_zlib": False
                 }
-                
+
    def build(self):
       cmake = CMake(self)
       cmake.configure()
