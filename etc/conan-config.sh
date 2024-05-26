62b7e5b20918e9530417121c391077e0d160ca2a
sascha <s.lumma@gmail.com>
2024-05-26 22:14:46 +0200

    Integrate conan profile creation into setup scripts

--- etc/conan-config.sh
+++ etc/conan-config.sh
@@ -3,15 +3,27 @@
 set -o errexit
 
 readonly WORKSPACE_ROOT="$(readlink -f $(dirname "$0"))"/../
-readonly COMPILER_NAME=${1}
-readonly COMPILER_VERSION=${2}
-readonly STANDARD_LIB=$(if [[ "$COMPILER_NAME" == "clang" ]]; then echo 'libc++'; else echo 'libstdc++11'; fi)
+COMPILER_NAME=${1}
+COMPILER_VERSION=${2}
 
 conan profile new ticket-decoder --force --detect
 
-conan profile update settings.compiler=${COMPILER_NAME} ticket-decoder
-conan profile update settings.compiler.version=${COMPILER_VERSION} ticket-decoder
-conan profile update settings.compiler.libcxx=${STANDARD_LIB} ticket-decoder
-conan profile update conf.tools.system.package_manager:mode=install ticket-decoder
+if [[ -z "${COMPILER_NAME}" ]]; then
+    COMPILER_NAME=$(conan profile get settings.compiler ticket-decoder)
+    if [[ -z "${COMPILER_NAME}" ]]; then
+        echo "ERROR: No compiler auto-dectected or passed as argument"
+        exit 1
+    fi
+else
+    conan profile update settings.compiler=${COMPILER_NAME} ticket-decoder
+fi
 
+if [[ ! -z ${COMPILER_VERSION} ]]; then
+    conan profile update settings.compiler.version=${COMPILER_VERSION} ticket-decoder
+fi
+
+readonly STANDARD_LIB=$(if [[ "$COMPILER_NAME" =~ ^(clang|apple-clang)$ ]]; then echo 'libc++'; else echo 'libstdc++11'; fi)
+conan profile update settings.compiler.libcxx=${STANDARD_LIB} ticket-decoder
+
+conan profile update conf.tools.system.package_manager:mode=install ticket-decoder
 conan profile show ticket-decoder
--- setup.Python.sh
+++ setup.Python.sh
@@ -4,8 +4,7 @@ set -o errexit
 
 readonly WORKSPACE_ROOT="$(readlink -f $(dirname "$0"))"
 
-# create a matching conan profile if it is not existing
-# conan profile new ticket-decoder --force --detect
+${WORKSPACE_ROOT}/etc/conan-config.sh
 ${WORKSPACE_ROOT}/etc/conan-install.sh Release -o:h with_analyzer=False
 ${WORKSPACE_ROOT}/etc/cmake-config.sh Release
 ${WORKSPACE_ROOT}/etc/cmake-build.sh Release -t ticket_decoder $@
