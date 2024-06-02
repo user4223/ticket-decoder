049914bf21eb69382533449c73e7c1d7961315bd
sascha <s.lumma@gmail.com>
2024-06-02 21:00:05 +0200

    Create internal sub-module instances

--- source/lib/api/source/DecoderFacade.cpp
+++ source/lib/api/source/DecoderFacade.cpp
@@ -3,14 +3,38 @@
 
 #include "lib/utility/include/Logging.h"
 
+#include "lib/io/api/include/Reader.h"
+#include "lib/io/api/include/Loader.h"
+
+#include "lib/dip/detection/api/include/Detector.h"
+#include "lib/dip/filtering/include/PreProcessor.h"
+
+#include "lib/uic918/api/include/Interpreter.h"
+
 namespace api
 {
     struct DecoderFacade::Internal
     {
+        std::vector<std::shared_ptr<io::api::Reader>> const readers;
+        io::api::Loader const loader;
+        dip::detection::api::Parameters parameters;
+        std::vector<std::shared_ptr<dip::detection::api::Detector>> const detector;
+        dip::utility::PreProcessor const preProcessor;
+        std::unique_ptr<uic918::api::Interpreter> const interpreter;
+
+        Internal(::utility::LoggerFactory &loggerFactory)
+            : readers(io::api::Reader::create(loggerFactory, io::api::ReadOptions{})),
+              loader(io::api::Loader(loggerFactory, readers)),
+              parameters(),
+              detector(dip::detection::api::Detector::createAll(loggerFactory, parameters)),
+              preProcessor(dip::utility::PreProcessor::create(loggerFactory, 0, "11")),
+              interpreter(uic918::api::Interpreter::create(loggerFactory))
+        {
+        }
     };
 
     DecoderFacade::DecoderFacade(::utility::LoggerFactory &loggerFactory)
-        : internal(std::make_shared<Internal>())
+        : internal(std::make_shared<Internal>(loggerFactory))
     {
     }
 }
