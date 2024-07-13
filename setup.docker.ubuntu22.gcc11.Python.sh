06120c5e4cf7e5e79e8e06b36d747e8a16564897
sascha <s.lumma@gmail.com>
2024-07-13 20:41:37 +0200

    Fix docker file names in calling build scripts 4 gcc

--- setup.docker.ubuntu22.gcc11.Python.sh
+++ setup.docker.ubuntu22.gcc11.Python.sh
@@ -5,7 +5,7 @@ set -o errexit
 readonly WORKSPACE_ROOT="$(readlink -f $(dirname "$0"))"
 
 mkdir -p ${WORKSPACE_ROOT}/build/ubuntu22.gcc11.Python
-docker buildx build ${WORKSPACE_ROOT} -t ubuntu22-gcc11-ticket-decoder-python-builder -f etc/docker/ubuntu22.gcc11.Python.Dockerfile  --build-arg="GCC_VERSION=11"
+docker buildx build ${WORKSPACE_ROOT} -t ubuntu22-gcc11-ticket-decoder-python-builder -f etc/docker/ubuntu22.gcc.Python.Dockerfile  --build-arg="GCC_VERSION=11"
 docker run -it \
   --mount type=bind,source=${WORKSPACE_ROOT}/source,target=/ticket-decoder/source,readonly \
   --mount type=bind,source=${WORKSPACE_ROOT}/images,target=/ticket-decoder/images,readonly \
