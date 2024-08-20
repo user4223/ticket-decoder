0666b5fcc78d027c90d8d81b0d04740e73de4799
sascha <s.lumma@gmail.com>
2024-08-20 16:13:22 +0200

    Make logger copyable and movable

--- source/lib/utility/include/Logger.h
+++ source/lib/utility/include/Logger.h
@@ -4,8 +4,22 @@
 
 namespace utility
 {
-  struct Logger
+  class LoggerFactory;
+
+  class Logger
   {
-    std::string const name;
+    std::string name;
+
+    Logger(std::string name);
+
+  public:
+    friend LoggerFactory;
+
+    Logger(Logger const &) = default;
+    Logger(Logger &&) = default;
+    Logger &operator=(Logger const &) = default;
+    Logger &operator=(Logger &&) = default;
+
+    char const *getName() const;
   };
 }
--- source/lib/utility/include/Logging.h
+++ source/lib/utility/include/Logging.h
@@ -7,8 +7,8 @@
 
 #define CREATE_LOGGER(factory) factory.fromPath(__FILE__)
 
-#define LOG_TRACE(logger) CLOG(TRACE, logger.name.c_str())
-#define LOG_DEBUG(logger) CLOG(DEBUG, logger.name.c_str())
-#define LOG_INFO(logger) CLOG(INFO, logger.name.c_str())
-#define LOG_WARN(logger) CLOG(WARNING, logger.name.c_str())
-#define LOG_ERROR(logger) CLOG(ERROR, logger.name.c_str())
+#define LOG_TRACE(logger) CLOG(TRACE, logger.getName())
+#define LOG_DEBUG(logger) CLOG(DEBUG, logger.getName())
+#define LOG_INFO(logger) CLOG(INFO, logger.getName())
+#define LOG_WARN(logger) CLOG(WARNING, logger.getName())
+#define LOG_ERROR(logger) CLOG(ERROR, logger.getName())
