d7732113b33cf3ec80def5cdf1b632faf612c749
sascha <s.lumma@gmail.com>
2024-12-25 19:25:58 +0100

    Increase version no

--- source/app/source/analyzer.cpp
+++ source/app/source/analyzer.cpp
@@ -20,7 +20,7 @@
 
 int main(int argc, char **argv)
 {
-    auto cmd = TCLAP::CmdLine("ticket-analyzer", ' ', "v0.9");
+    auto cmd = TCLAP::CmdLine("ticket-analyzer", ' ', "v0.10");
     auto const verboseArg = TCLAP::SwitchArg(
         "v", "verbose",
         "More verbose debug logging",
