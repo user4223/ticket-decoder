577b3ad6de47dcf6dc6d227f7e3b962de58108f9
sascha <s.lumma@gmail.com>
2025-04-25 21:10:24 +0200

    Fix docker image with ubuntu24 and clang16 and update docs

--- setup.docker.ubuntu24.clang16.sh
+++ setup.docker.ubuntu24.clang16.sh
@@ -4,11 +4,11 @@ set -o errexit
 
 readonly WORKSPACE_ROOT="$(readlink -f $(dirname "$0"))"
 
-mkdir -p ${WORKSPACE_ROOT}/build/ubuntu22.clang15.Release
-docker buildx build ${WORKSPACE_ROOT} -t ubuntu22-clang15-ticket-decoder-builder -f etc/docker/ubuntu22.clang.Dockerfile --build-arg="CLANG_VERSION=15"
+mkdir -p ${WORKSPACE_ROOT}/build/ubuntu24.clang16.Release
+docker buildx build ${WORKSPACE_ROOT} -t ubuntu24-clang16-ticket-decoder-builder -f etc/docker/ubuntu24.clang.Dockerfile --build-arg="CLANG_VERSION=16"
 docker run -it \
   --mount type=bind,source=${WORKSPACE_ROOT}/source,target=/ticket-decoder/source,readonly \
   --mount type=bind,source=${WORKSPACE_ROOT}/images,target=/ticket-decoder/images,readonly \
   --mount type=bind,source=${WORKSPACE_ROOT}/CMakeLists.txt,target=/ticket-decoder/CMakeLists.txt,readonly \
-  --mount type=bind,source=${WORKSPACE_ROOT}/build/ubuntu22.clang15.Release,target=/ticket-decoder/build/Release/bin \
-  ubuntu22-clang15-ticket-decoder-builder
+  --mount type=bind,source=${WORKSPACE_ROOT}/build/ubuntu24.clang16.Release,target=/ticket-decoder/build/Release/bin \
+  ubuntu24-clang16-ticket-decoder-builder
