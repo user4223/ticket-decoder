62343337a21db53eec0f9e4b4d19bada6fbb29b5
sascha <s.lumma@gmail.com>
2025-10-12 17:03:51 +0200

    Migrate local libpoppler recipe and build script 2 conan2

--- etc/poppler/conan-create.sh
+++ etc/poppler/conan-create.sh
@@ -7,8 +7,8 @@ readonly BUILD_TYPE=${1:-Release}
 
 conan create --build=missing \
     -pr ticket-decoder -pr:b ticket-decoder \
-    -s build_type=${BUILD_TYPE} \
-    -e CMAKE_POLICY_VERSION_MINIMUM=3.5 \
+    -s:b "build_type=${BUILD_TYPE}" \
+    -o:b "*:CMAKE_POLICY_VERSION_MINIMUM=3.5" \
     ${WORKSPACE_ROOT}/etc/poppler
 
-conan remove 'poppler-cpp' --build --src --force
+conan cache clean '*'
