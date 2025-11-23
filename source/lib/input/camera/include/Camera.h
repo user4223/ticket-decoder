23392688713f5a41d5d6d0356deb34dd4c75b7e5
sascha <s.lumma@gmail.com>
2025-11-23 15:08:08 +0100

    Add license headers 2 each non-generated header/source file

--- source/lib/input/camera/include/Camera.h
+++ source/lib/input/camera/include/Camera.h
@@ -1,3 +1,6 @@
+// SPDX-FileCopyrightText: (C) 2022 user4223 and (other) contributors to ticket-decoder <https://github.com/user4223/ticket-decoder>
+// SPDX-License-Identifier: GPL-3.0-or-later
+
 #pragma once
 
 #include <opencv2/core/mat.hpp>
--- source/test/infrastructure/source/ContextTest.cpp
+++ source/test/infrastructure/source/ContextTest.cpp
@@ -1,3 +1,6 @@
+// SPDX-FileCopyrightText: (C) 2022 user4223 and (other) contributors to ticket-decoder <https://github.com/user4223/ticket-decoder>
+// SPDX-License-Identifier: GPL-3.0-or-later
+
 #include <gtest/gtest.h>
 
 #include "lib/infrastructure/include/Context.h"
