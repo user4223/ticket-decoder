4847dfc136375fbea6ea9997468692af4dd204d1
sascha <s.lumma@gmail.com>
2026-04-18 23:55:29 +0200

    Add CSV stuff to json output

--- source/lib/interpreter/detail/misc/source/CSVInterpreter.cpp
+++ source/lib/interpreter/detail/misc/source/CSVInterpreter.cpp
@@ -11,17 +11,19 @@
 
 namespace interpreter::detail::misc
 {
-    void split(std::string_view str, char const delimiter, std::insert_iterator<std::vector<std::string_view>> &&inserter)
+    constexpr std::vector<std::string_view> split(std::string_view str, char const delimiter)
     {
+        auto parts = std::vector<std::string_view>{};
         auto pos = std::size_t(0);
         while (pos < str.size())
         {
             auto const next_pos = str.find_first_of(delimiter, pos);
-            inserter = str.substr(pos, next_pos - pos);
+            parts.push_back(str.substr(pos, next_pos - pos));
             pos = next_pos == std::string_view::npos
                       ? str.size()
                       : next_pos + 1;
         }
+        return parts;
     }
 
     CSVInterpreter::CSVInterpreter(infrastructure::LoggerFactory &lf)
@@ -43,22 +45,21 @@ namespace interpreter::detail::misc
     common::Context CSVInterpreter::interpret(common::Context &&context)
     {
         auto const content = common::StringDecoder::decodeASCII(context.consumeRemainingBytes(), true);
-
-        auto parts = std::vector<std::string_view>{};
-        split(content, ';', std::inserter(parts, parts.begin()));
+        auto const parts = split(content, ';');
 
         auto recordJson = utility::JsonBuilder::object();
         if (parts.size() == 16)
         {
-            auto const price = common::NumberDecoder::decodeInteger(parts[3]);
-            auto const validFromDate = common::DateTimeDecoder::decodeDate6(parts[5]);
-            auto const validUntilDate = common::DateTimeDecoder::decodeDate6(parts[6]);
-            auto const passengerNumber = common::NumberDecoder::decodeInteger(parts[7]);
-            auto const ticketId = parts[8];
-            auto const issuingDate = common::DateTimeDecoder::decodeDate6(parts[11]);
-            auto const fromStationIBNR = parts[12];
-            auto const toStationIBNR = parts[13];
-            auto const classCode = parts[15];
+            recordJson
+                .add("price", common::NumberDecoder::decodeInteger(parts[3]))
+                .add("validFromDate", common::DateTimeDecoder::decodeDate6(parts[5]))
+                .add("validUntilDate", common::DateTimeDecoder::decodeDate6(parts[6]))
+                .add("passengerNumber", common::NumberDecoder::decodeInteger(parts[7]))
+                .add("ticketId", parts[8])
+                .add("issuingDate", common::DateTimeDecoder::decodeDate6(parts[11]))
+                .add("fromStationIBNR", parts[12])
+                .add("toStationIBNR", parts[13])
+                .add("classCode", parts[15]);
         }
 
         context.addRecord(common::Record("CSV", "", std::move(recordJson)));
